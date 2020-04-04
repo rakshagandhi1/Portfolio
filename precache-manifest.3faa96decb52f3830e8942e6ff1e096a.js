58a1884072fd16898709f30af0301483e2b1229c
Raksha <rakshagandhi1997@gmail.com>
2020-04-04 22:03:19 +0530

    Updates

---
+++ precache-manifest.3faa96decb52f3830e8942e6ff1e096a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cff7c560e41433e3142028f16869b14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47d90f7cc81a27eac14c",
+    "url": "/portfolio/static/css/main.f663a84e.chunk.css"
+  },
+  {
+    "revision": "6ff16c129a21092a8375",
+    "url": "/portfolio/static/js/2.1fe023a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1fe023a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d90f7cc81a27eac14c",
+    "url": "/portfolio/static/js/main.e43bfc9f.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "8430bcc8cbff4697aadc3a892ea9632a",
+    "url": "/portfolio/static/media/background.8430bcc8.jpg"
+  },
+  {
+    "revision": "6cc363a4036adeebff4a43d9fe38123c",
+    "url": "/portfolio/static/media/email.6cc363a4.png"
+  },
+  {
+    "revision": "4ade2d9a1b28503ea4d9c5b2172d1355",
+    "url": "/portfolio/static/media/mobile.4ade2d9a.png"
+  },
+  {
+    "revision": "93a9d0a7a09cb42f396662cd77256711",
+    "url": "/portfolio/static/media/resume.93a9d0a7.pdf"
+  }
+]);
